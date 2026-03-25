b6668d0822b960beb82ad8fffbf272ae648c624e
holicepc <103413331+holicepc@users.noreply.github.com>
2026-03-25 11:33:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="4.6.7"
-DOWNLOAD_HOST="https://github.com/EvilGenius-dot/shortcut/raw/main/Readme/92/linux"
+DOWNLOAD_HOST="https://github.com/holicepc/EcMinerSystem/raw/main/linux"
 ORIGIN_EXEC="ecminer-${VERSION}"
 
 SERVICE_NAME="ecminerservice"
